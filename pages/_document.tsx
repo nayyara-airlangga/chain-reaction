4b1bdb19c0f99e44b9e1c6428ec0edda08f24d84
nayyara-airlangga <nayyara.airlangga@gmail.com>
2022-01-02 00:36:50 +0700

    Change favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,6 +27,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/assets/images/logo.png" />
         </Head>
         <body>
           <Main />
